ced155dc6062e36647c53662c956cb3ddf596957
DerToti <dertoti@gmail.com>
2020-03-11 00:07:45 +0100

    Prepared index tsx

--- src/index.tsx
+++ src/index.tsx
@@ -4,18 +4,11 @@ import './index.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-// import App from './00-selectors/solution/App';
-// import App from './00-selectors-2/solution/App';
-// import App from './00-selectors-3/solution/App';
-// import App from './00-selectors-pseudoclasses/solution/App';
-// import App from './01-basic-css/solution/App';
-// import App from './02-css-integration/solution/App';
-// import App from './03-reactstrap/solution/App';
-// import App from './04-grid/solution/App';
 /**
  * Exercises
  */
-// import App from './00-selectors/App';
+
+import App from './00-selectors/App';
 // import App from './00-selectors-2/App';
 // import App from './00-selectors-3/App';
 // import App from './00-selectors-pseudoclasses/App';
@@ -24,6 +17,5 @@ import ReactDOM from 'react-dom';
 // import App from './03-reactstrap/App';
 // import App from './04-grid/App';
 // import App from './05-grid-responsive/App';
-import App from './99-example/App';
 
 ReactDOM.render(<App />, document.getElementById('root'));
