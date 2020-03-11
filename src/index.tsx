f4b8dfba3777107544546ef9ed9c585a940b15a7
DerToti <dertoti@gmail.com>
2020-03-11 14:38:08 +0100

    Removed unused exercise

--- src/index.tsx
+++ src/index.tsx
@@ -18,6 +18,5 @@ import App from './01-selectors-1/App';
 // import App from './04-css-integration/App';
 // import App from './05-reactstrap/App';
 // import App from './06-grid/App';
-// import App from './07-grid-responsive/App';
 
 ReactDOM.render(<App />, document.getElementById('root'));
